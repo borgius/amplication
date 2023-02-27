90f914c6ed42804c8251d33f125c6bd1d06c7e73
Viktor <868919+borgius@users.noreply.github.com>
2023-02-27 08:45:40 -0500

    lint

--- packages/amplication-data-service-generator-runner/scripts/link-plugins.sh
+++ packages/amplication-data-service-generator-runner/scripts/link-plugins.sh
@@ -3,21 +3,18 @@ BASE=$(realpath "${BASH_SOURCE[0]:-${0}}/../..")
 ROOT=$(realpath "$BASE/../..")
 PLUGINS="$ROOT/libs/plugins/plugins"
 DST="$BASE/amplication_modules/@amplication"
-echo $BASE
-echo $ROOT
-echo $DST
 rm -rf $DST
 mkdir -p $DST
 for pkg in $PLUGINS/*; do
-  plugin=$(basename $pkg)
-  test -d $pkg/node_modules || {
-    cd $pkg 
-    npm i
-  }
-  test -d $pkg/dist || {
-    cd $pkg 
-    npm run build
-  }
+    plugin=$(basename $pkg)
+    test -d $pkg/node_modules || {
+        cd $pkg
+        npm i
+    }
+    test -d $pkg/dist || {
+        cd $pkg
+        npm run build
+    }
 
-  ln -s "$pkg" "$DST/plugin-$plugin"
+    ln -s "$pkg" "$DST/plugin-$plugin"
 done
