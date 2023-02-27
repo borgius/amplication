a38c3d1f755da67ac1cb39f51f23ae43391dbca7
Viktor <868919+borgius@users.noreply.github.com>
2023-02-27 08:45:59 -0500

    lint

--- packages/amplication-data-service-generator-runner/scripts/link-plugins.sh
+++ packages/amplication-data-service-generator-runner/scripts/link-plugins.sh
@@ -6,15 +6,15 @@ DST="$BASE/amplication_modules/@amplication"
 rm -rf $DST
 mkdir -p $DST
 for pkg in $PLUGINS/*; do
-    plugin=$(basename $pkg)
-    test -d $pkg/node_modules || {
-        cd $pkg
-        npm i
-    }
-    test -d $pkg/dist || {
-        cd $pkg
-        npm run build
-    }
+  plugin=$(basename $pkg)
+  test -d $pkg/node_modules || {
+    cd $pkg
+    npm i
+  }
+  test -d $pkg/dist || {
+    cd $pkg
+    npm run build
+  }
 
-    ln -s "$pkg" "$DST/plugin-$plugin"
+  ln -s "$pkg" "$DST/plugin-$plugin"
 done
